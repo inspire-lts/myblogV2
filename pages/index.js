54b8e737bfdcd302e2ffc93fd54054fd52ed74da
inspire-lts <1183132919@qq.com>
2022-01-30 10:31:15 +0800

    fix: avatar url and same key

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,48 @@
-import { Divider, Flex, Heading, HStack, Text, Box } from "@chakra-ui/layout"
-import { Container, Image } from "@chakra-ui/react"
-import ArticleLists from "../components/ArticleLists"
-import { getPosts } from "../lib/data"
+import { Divider, Flex, Heading, HStack, Text, Box } from "@chakra-ui/layout";
+import { Container, Image } from "@chakra-ui/react";
+import ArticleLists from "../components/ArticleLists";
+import { getPosts } from "../lib/data";
 
-export default function Home({ data: { posts} }) {
-  const recentPosts = posts.slice(0, 3)
+export default function Home({ data: { posts } }) {
+  const recentPosts = posts.slice(0, 3);
+
   return (
     <Container maxW="container.sm" p={4}>
       <HStack justifyContent="center">
-        <Box p={5} >
-          <Heading as="h2" variant="page-title">小炒肉</Heading>
+        <Box p={5}>
+          <Heading as="h2" variant="page-title">
+            小炒肉
+          </Heading>
           <p>前端练习生 🧐 前端开发者</p>
         </Box>
         <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/avator.jpg"
-            alt="Profile image"
-          />
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          maxWidth="100px"
+          display="inline-block"
+          borderRadius="full"
+          src={recentPosts[0].author.image.url}
+          alt="Profile image"
+        />
       </HStack>
       <Text fontSize="2xl">近期文章</Text>
-      <Divider/>
+      <Divider />
       <Flex justifyContent="center" direction="column">
-        {
-          recentPosts?.map((post) => {
-            return (
-              <ArticleLists {...post} key={post}/>
-            )
-          })
-        }
+        {recentPosts?.map((post) => {
+          return <ArticleLists {...post} key={post.slug} />;
+        })}
       </Flex>
     </Container>
-  )
+  );
 }
 
-export const getStaticProps = async() => {
-  const data = await getPosts()
+export const getStaticProps = async () => {
+  const data = await getPosts();
 
   return {
     props: {
-      data
-    }
-  }
-}
+      data,
+    },
+  };
+};
